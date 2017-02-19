7072133221adb6546d007ad627719222a825e18e
崔大鹏 <cclient@android5.local>
2017-02-19 12:53:28 +0800

    first update

---
+++ CodeSmithTemplates/C#/DBHelper.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Reflection;
+
+
+    public static class DBHelper
+    {
+
+        private static SqlConnection connection;
+        public static SqlConnection Connection
+        {
+            get 
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+                if (connection == null)
+                {
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+                }
+                else if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                else if (connection.State == System.Data.ConnectionState.Broken)
+                {
+                    connection.Close();
+                    connection.Open();
+                }
+                return connection;
+            }
+        }
+
+
+        public static int ExecuteCommand(string safeSql)
+        {
+            SqlCommand cmd = new SqlCommand(safeSql, Connection);
+            int result = cmd.ExecuteNonQuery();
+            return result;
+        }
+
+        public static int ExecuteCommand(string sql, params SqlParameter[] values)
+        {
+            SqlCommand cmd = new SqlCommand(sql, Connection);
+            cmd.Parameters.AddRange(values);
+            return cmd.ExecuteNonQuery();
+        }
+
+        public static int GetScalar(string safeSql)
+        {
+            SqlCommand cmd = new SqlCommand(safeSql, Connection);
+            int result = Convert.ToInt32(cmd.ExecuteScalar());
+            return result;
+        }
+
+        public static int GetScalar(string sql, params SqlParameter[] values)
+        {
+            SqlCommand cmd = new SqlCommand(sql, Connection);
+            cmd.Parameters.AddRange(values);
+            int result = Convert.ToInt32(cmd.ExecuteScalar());
+            return result;
+        }
+
+        public static SqlDataReader GetReader(string safeSql)
+        {
+            SqlCommand cmd = new SqlCommand(safeSql, Connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+
+        public static SqlDataReader GetReader(string sql, params SqlParameter[] values)
+        {
+            SqlCommand cmd = new SqlCommand(sql, Connection);
+            cmd.Parameters.AddRange(values);
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+
+        public static DataTable GetDataSet(string safeSql)
+        {
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand(safeSql, Connection);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+
+        public static DataTable GetDataSet(string sql, params SqlParameter[] values)
+        {
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand(sql, Connection);
+            cmd.Parameters.AddRange(values);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+
+        public static List<T> GetList<T>(string sql)
+        {
+            DataTable table = GetDataSet(sql);
+            List<T> list = new List<T>();
+            T t = default(T);
+            PropertyInfo[] propertypes = null;
+            string tempName = string.Empty;
+            foreach (DataRow row in table.Rows)
+            {
+                t = Activator.CreateInstance<T>();
+                propertypes = t.GetType().GetProperties();
+                foreach (PropertyInfo pro in propertypes)
+                {
+                    tempName = pro.Name;
+                    if (table.Columns.Contains(tempName))
+                    {
+                        object value = row[tempName];
+                        try
+                        {
+                            pro.SetValue(t, value, null);
+                        }
+                        catch { }
+                    }
+                }
+                list.Add(t);
+            }
+            return list;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # codesmithdemo
 定制的代码生成器模版，可以作参考，语言目前只有C# 和GO的例子。
+
+简介
+http://www.cnblogs.com/zihunqingxin/p/6415500.html
+
+codesmith官网
+http://www.codesmithtools.com/
+
+go的例子应用在
+https://github.com/cclient/gowebframework/
